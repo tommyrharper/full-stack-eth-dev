2293a08ad309f9d2afeb2efb5bfdb89dc0e3ab6a
tommyrharper <thomasrobertharper@gmail.com>
2022-10-22 19:49:22 -0500

    feat: add Token contract to deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,16 +1,26 @@
 const hre = require("hardhat");
 
-async function main() {  
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello World");
-  await greeter.deployed();
+async function main() {
+  const [deployer] = await hre.ethers.getSigners();
 
-  console.log(
-    `contract successfully deployed to ${greeter.address}`
-  );
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  const Greeter = await hre.ethers.getContractFactory("Greeter");
+  const greeter = await Greeter.deploy("Hello, World!");
+
+  const Token = await hre.ethers.getContractFactory("Token");
+  const token = await Token.deploy();
+
+  await greeter.deployed();
+  await token.deployed();
+
+  console.log("Greeter deployed to:", greeter.address);
+  console.log("Token deployed to:", token.address);
 }
 
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
